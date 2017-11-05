7af4a88645e9a4259e253fa054dc7c61f533837d
IbtesamDheir <ibtesam.dh2@gmail.com>
2017-11-05 08:34:39 +0200

    5.1 Update site root + Added routes to point to custom page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,25 @@
 Rails.application.routes.draw do
-  resources :products
-  get 'static_pages/about'
 
-  get 'static_pages/contact'
+  resources :products
+
+  resources :orders, only: [:index, :show, :create, :destroy]
+
+ 
+  # root
+  root 'static_pages#index'
+
+
+  # static_pages routes
+  get 'static_pages/', to: 'static_pages#index' 
 
   get 'static_pages/index'
-  root 'static_pages#landing_page'
+
+  get 'static_pages/about'
+
+  get 'static_pages/contact', to: 'static_pages#contact'
+
+  get 'static_pages/landing_page'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :orders, only: [:index, :show, :create, :destroy]
 
 end
