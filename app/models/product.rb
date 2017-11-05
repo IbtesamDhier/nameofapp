238984f33bbb46317b12a93704cf29a55347c091
IbtesamDheir <ibtesam.dh2@gmail.com>
2017-11-05 13:16:34 +0200

    5.2 Add search form using form helper + styling forms using bootstrap

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,15 @@
 class Product < ApplicationRecord
 	has_many :orders
+
+
+
+	def self.search(search_term)
+    	search_term.strip! #Returns a copy of str with leading and trailing whitespace removed.
+    	if (Rails.env.development? || Rails.env.test?)
+      		Product.where('name LIKE ?', "%#{search_term}%")
+    	else
+      		Product.where('name ilike ?', "%#{search_term}%")
+    	end
+	end
+
 end
