b9f4e6e56fa07d0df3763b4b675206667ac78a4d
IbtesamDheir <ibtesam.dh2@gmail.com>
2017-11-15 09:07:22 +0200

    5.3 Testing sending emails in production env......

--- config/environment.rb
+++ config/environment.rb
@@ -9,9 +9,8 @@ ActionMailer::Base.smtp_settings = {
   address: 'smtp.sendgrid.net',
   port: '587',
   authentication: :plain,
-  user_name: ENV['app80085053@heroku.com'],
+  user_name: 'app80085053@heroku.com',
   password: ENV['sam2152011*'],
-  domain: 'heroku.com',
   enable_starttls_auto: true
 }
 
