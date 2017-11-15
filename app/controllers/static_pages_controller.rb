3ff54f5c5fdf7d32cc7a6c6f05ed026763d3d2ad
IbtesamDheir <ibtesam.dh2@gmail.com>
2017-11-15 13:47:00 +0200

    5.3 Testing sending emails in production envi......

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,10 +10,29 @@ class StaticPagesController < ApplicationController
   @name = params[:name]
   @email = params[:email]
   @message = params[:message]
+  #UserMailer.contact_form(@email, @name, @message).deliver_now
   ActionMailer::Base.mail(from: @email,
       to: 'ibtesam.dh2@gmail.com',
       subject: "A new contact form message from #{@name}",
       body: @message).deliver_now
+
+
+def create
+  @user = User.new(user_params)
+
+  respond_to do |format|
+    if @user.save
+
+      # Sends email to user when user is created.
+      ExampleMailer.sample_email(@user).deliver
+
+      format.html { redirect_to @user, notice: 'User was successfully created.' }
+      format.json { render :show, status: :created, location: @user }
+    else
+      format.html { render :new }
+      format.json { render json: @user.errors, status: :unprocessable_entity }
+    end
   end
+end
 
 end
--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,13 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-
 ActionMailer::Base.smtp_settings = {
   address: 'smtp.sendgrid.net',
   port: '587',
   authentication: :plain,
-  user_name: 'app80085053@heroku.com',
-  password: ENV['sam2152011*'],
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
+  domain: 'heroku.com',
   enable_starttls_auto: true
 }
 
