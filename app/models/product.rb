dc4c766ea1254cf32bedceb9e2382da554da2a7f
IbtesamDheir <ibtesam.dh2@gmail.com>
2017-11-17 20:00:26 +0200

    5.7 creat comment model&set up scopes and custom methods & styling product/show page

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,6 @@ class Product < ApplicationRecord
 	has_many :orders
 	has_many :comments
 
-
 	def self.search(search_term)
     	search_term.strip! #Returns a copy of str with leading and trailing whitespace removed.
     	if (Rails.env.development? || Rails.env.test?)
@@ -10,6 +9,14 @@ class Product < ApplicationRecord
     	else
       		Product.where('name ilike ?', "%#{search_term}%")
     	end
+	end
+
+	def highest_rating_comment
+  		comments.rating_desc.first
+	end
+
+ 	def lowest_rating_comment
+    	comments.rating_asc.first
 	end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171117100908) do
+ActiveRecord::Schema.define(version: 20171117153550) do
 
   create_table "comments", force: :cascade do |t|
-    t.float "rating"
-    t.string "message"
+    t.integer "user_id"
+    t.text "body"
+    t.integer "rating"
+    t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_comments_on_product_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "orders", force: :cascade do |t|
