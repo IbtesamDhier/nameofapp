a340a6b954fb4a38c2889be3c9127e6aaa42c2ed
IbtesamDheir <ibtesam.dh2@gmail.com>
2017-11-18 14:24:05 +0200

    5.7 creating comment model & set up scopes and custom methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,4 +12,12 @@ class Product < ApplicationRecord
     	end
 	end
 
+	def highest_rating_comment
+  		comments.rating_desc.first
+	end
+
+	def lowest_rating_comment
+  		comments.rating_asc.first
+	end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 	
     has_many :orders
-    has_one :comment
+    has_one :comments
+
 
 end
