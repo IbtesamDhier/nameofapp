c2aed13bbcfc01f9d3b79054389b347087a49112
IbtesamDheir <ibtesam.dh2@gmail.com>
2017-12-04 20:43:12 +0200

    6.12 using Redis as a cache store

--- Gemfile
+++ Gemfile
@@ -76,3 +76,7 @@ gem 'stripe'
 gem "brakeman", :require => false
 
 gem 'dalli'
+
+gem 'redis-rails'
+
+gem 'newrelic_rpm'
--- app/models/product.rb
+++ app/models/product.rb
@@ -27,4 +27,20 @@ class Product < ApplicationRecord
   		comments.average(:rating).to_f
 	end
 
+	def views
+        $redis.get("product:#{id}") # this is equivalent to 'GET product:1'
+    end
+
+    def viewed!
+        $redis.incr("product:#{id}") # this is equivalent to 'INC product:1'
+    end
+
+    def set_latest_reviewer(user_id)
+		$redis.set("lr_product:#{id}","#{user_id}")
+	end
+
+    def get_latest_reviewer
+    	$redis.get("lr_product:#{id}") # this returns the user's first name...
+    end
+
 end
