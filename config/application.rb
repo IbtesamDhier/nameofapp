9b98ed798d66f505f43a10190d2e2f8bdc8e851d
IbtesamDheir <ibtesam.dh2@gmail.com>
2017-12-10 08:46:54 +0200

    fixing

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,6 @@ module Nameofapp
     config.cache_store = :redis_store, 'redis://localhost:6379/0/cache'
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.serve_static_assets = true
   end
 end
