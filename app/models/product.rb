bc0a12ef2c57f9d53e113eb17dcbbf42a28133da
IbtesamDheir <ibtesam.dh2@gmail.com>
2017-12-30 17:53:52 +0200

    creating controller actions and views for orders

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,32 +5,32 @@ class Product < ApplicationRecord
   validates :name, presence: true
 	
   def self.search(search_term)
-    	search_term.strip! #Returns a copy of str with leading and trailing whitespace removed.
-    	if (Rails.env.development? || Rails.env.test?)
-      		Product.where('name LIKE ?', "%#{search_term}%")
-    	else
-      		Product.where('name ilike ?', "%#{search_term}%")
-    	end
+    search_term.strip! #Returns a copy of str with leading and trailing whitespace removed.
+    if (Rails.env.development? || Rails.env.test?)
+      Product.where('name LIKE ?', "%#{search_term}%")
+    else
+      Product.where('name ilike ?', "%#{search_term}%")
+    end
 	end
 
 	def highest_rating_comment
-  		comments.rating_desc.first
+  	comments.rating_desc.first
 	end
 
 	def lowest_rating_comment
-  		comments.rating_asc.first
+  	comments.rating_asc.first
 	end
 
 	def average_rating
-  		comments.average(:rating).to_f
+  	comments.average(:rating).to_f
 	end
 
 	def views
-        $redis.get("product:#{id}") # this is equivalent to 'GET product:1'
-    end
+    $redis.get("product:#{id}") # this is equivalent to 'GET product:1'
+  end
 
-    def viewed!
-        $redis.incr("product:#{id}") # this is equivalent to 'INC product:1'
-    end
+  def viewed!
+    $redis.incr("product:#{id}") # this is equivalent to 'INC product:1'
+  end
 
 end
